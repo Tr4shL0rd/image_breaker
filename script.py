0f1e6f55682f7aa720bc4e7ccff10dc97c88232e
Tr4shL0rd <ketil04@gmail.com>
2023-02-24 08:38:46 +0100

    now uses a class for tqdm progress bar

--- script.py
+++ script.py
@@ -24,6 +24,7 @@ NOTICE  = "[yellow][!][/yellow]"
 #TODO: maybe edit metadata to show what manipulations has been done to the image
 #TODO: allow multiple files to be given from command line and loop over each file
 #TODO: add a better way to write the TQDM progress bar, instead of repeatedly rewriting the same progress bar
+#TODO: add flags for each image filter
 
 
 parser = argparse.ArgumentParser(
@@ -68,6 +69,25 @@ parser.add_argument(
                     help="name of the file outputted"
                     )
 args = parser.parse_args()
+
+class TqdmWrapper:
+    def __init__(self, desc="shifting", total=None, disable=False, quiet=False):
+        self.desc = desc
+        self.total = total
+        self.disable = disable or quiet
+
+    def __enter__(self):
+        self.pbar = tqdm(
+            total=self.total,
+            desc=self.desc,
+            bar_format="{desc}: {percentage:3.0f}% |{bar}|",
+            leave=False,
+            disable=self.disable
+        )
+        return self.pbar
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.pbar.close()
 
 def current_time(just_time=False,just_date=False) -> str:
     """
@@ -115,33 +135,30 @@ def noise(image:Image) -> List:
     img_pixels = list(image.getdata())
     
     _new_pixels = []
-    for pixel in tqdm(
-                    img_pixels,
-                    total=len(img_pixels),
-                    desc="Adding noise",
-                    bar_format="{desc}: {percentage:3.0f}% |{bar}|",
-                    leave=False,
-                    disable=args.no_progress or args.quiet):
-        # RNG for adding or subtracting pixel brightness
-        pixel_brightness_rng = random.randint(1,2)
-        # RNG to determine if a pixel should be manipulated
-        manipulate_pixel_rng = random.randint(1,50)
-        if manipulate_pixel_rng < 10:
-            # Subtract or add random values from the color channels to create a "broken" pixel
-            broken_pixel:tuple = (
-                                pixel[0] - random.randint(0,100)
-                                    if pixel_brightness_rng == 1 else
-                                        pixel[0] + random.randint(0,100),
-                                pixel[1] - random.randint(0,100)
-                                    if pixel_brightness_rng == 1 else
-                                        pixel[1] + random.randint(0,100),
-                                pixel[2] - random.randint(0,100)
-                                    if pixel_brightness_rng == 1 else
-                                        pixel[2] + random.randint(0,100)
-                                )
-        else:
-            broken_pixel = (pixel[0],pixel[1],pixel[2])
-        _new_pixels.append(broken_pixel)
+    #for pixel in tqdm(img_pixels,total=len(img_pixels),desc="Adding noise",bar_format="{desc}: {percentage:3.0f}% |{bar}|",leave=False,disable=args.no_progress or args.quiet):
+    with TqdmWrapper(desc="Adding Noise", total=len(img_pixels), disable=args.no_progress or args.quiet) as pbar:
+        for pixel in img_pixels:
+            # RNG for adding or subtracting pixel brightness
+            pixel_brightness_rng = random.randint(1,2)
+            # RNG to determine if a pixel should be manipulated
+            manipulate_pixel_rng = random.randint(1,50)
+            if manipulate_pixel_rng < 10:
+                # Subtract or add random values from the color channels to create a "broken" pixel
+                broken_pixel:tuple = (
+                                    pixel[0] - random.randint(0,100)
+                                        if pixel_brightness_rng == 1 else
+                                            pixel[0] + random.randint(0,100),
+                                    pixel[1] - random.randint(0,100)
+                                        if pixel_brightness_rng == 1 else
+                                            pixel[1] + random.randint(0,100),
+                                    pixel[2] - random.randint(0,100)
+                                        if pixel_brightness_rng == 1 else
+                                            pixel[2] + random.randint(0,100)
+                                    )
+            else:
+                broken_pixel = (pixel[0],pixel[1],pixel[2])
+            _new_pixels.append(broken_pixel)
+            pbar.update(1)
     return _new_pixels
 
 def shift(image:Image) -> List:
@@ -165,29 +182,25 @@ def shift(image:Image) -> List:
     width, height = image.size
     img_pixels = list(image.getdata()) # RGB data of each pixel
     _new_pixels = []
-    for _y in tqdm(
-                    range(height),
-                    total=height,
-                    desc="shifting",
-                    bar_format="{desc}: {percentage:3.0f}% |{bar}|",
-                    leave=False, # removes the progress bar after finishing
-                    # Disables the progress bar if quiet
-                    disable=args.no_progress or args.quiet):
-        # Generate a random integer between 1 and 100
-        manipulate_pixel_chance_rng = random.randint(1,100)
-        # Generate a random integer between 0 and the width of the image
-        shift_distance_rng = random.randint(0,width)
-        for _x in range(width):
-            # checks if the pixels is to be shifted
-            if manipulate_pixel_chance_rng < 10:
-                # Calculate the new x and y positions for the shifted pixel
-                new_x = (_x + shift_distance_rng) % width
-                new_y = (_y + shift_distance_rng) % height
-                # Append the shifted pixel to the new_pixels list
-                _new_pixels.append(img_pixels[new_y * width + new_x])
-            else:
-                # Append the original pixel to the new_pixels list
-                _new_pixels.append(img_pixels[_y * width + _x])
+    #for _y in tqdm(range(height),total=height,desc="shifting",bar_format="{desc}: {percentage:3.0f}% |{bar}|",leave=False, # removes the progress bar after finishing# Disables the progress bar if quietdisable=args.no_progress or args.quiet):
+    with TqdmWrapper(desc="Shifting", total=height, disable=args.no_progress or args.quiet) as pbar:
+        for _y in range(height):
+            # Generate a random integer between 1 and 100
+            manipulate_pixel_chance_rng = random.randint(1,100)
+            # Generate a random integer between 0 and the width of the image
+            shift_distance_rng = random.randint(0,width)
+            for _x in range(width):
+                # checks if the pixels is to be shifted
+                if manipulate_pixel_chance_rng < 10:
+                    # Calculate the new x and y positions for the shifted pixel
+                    new_x = (_x + shift_distance_rng) % width
+                    new_y = (_y + shift_distance_rng) % height
+                    # Append the shifted pixel to the new_pixels list
+                    _new_pixels.append(img_pixels[new_y * width + new_x])
+                else:
+                    # Append the original pixel to the new_pixels list
+                    _new_pixels.append(img_pixels[_y * width + _x])
+            pbar.update(1)
     return _new_pixels
 
 def duplicate(image: Image, grid_size:int=4, chance:int|None=None) -> List:
@@ -226,29 +239,24 @@ def duplicate(image: Image, grid_size:int=4, chance:int|None=None) -> List:
     if chance is None:
         chance = 5
 
-    for _y in tqdm(
-                    range(height),
-                    total=height,
-                    desc="duplicating",
-                    bar_format="{desc}: {percentage:3.0f}% |{bar}|",
-                    leave=False,  # removes the progress bar after finishing
-                    # Disables the progress bar if quiet
-                    disable=args.no_progress or args.quiet):
-
-        for _x in range(width):
-            manipulate_pixel_chance_rng = random.randint(1, 100)
-            # checks if the pixels is to be duplicated
-            if manipulate_pixel_chance_rng < chance:
-                # Generate a random integer between 0 and the width-grid_size of the image
-                x_start_rng = random.randint(0, width  - grid_size)
-                y_start_rng = random.randint(height//2, height - grid_size)
-                for new_y in range(y_start_rng,     y_start_rng + grid_size):
-                    for new_x in range(x_start_rng, x_start_rng + grid_size):
-                        # Append the duplicated pixel to the new_pixels list
-                        _new_pixels.append(img_pixels[new_y * width + new_x])
-            else:
-                #pass
-                _new_pixels.append(img_pixels[_y * width + _x])
+    #for _y in tqdm(range(height),total=height,desc="duplicating",bar_format="{desc}: {percentage:3.0f}% |{bar}|",leave=False,  # removes the progress bar after finishing# Disables the progress bar if quietdisable=args.no_progress or args.quiet):
+    with TqdmWrapper(desc="Duplicating", total=height, disable=args.no_progress or args.quiet) as pbar:
+        for _y in range(height):
+            for _x in range(width):
+                manipulate_pixel_chance_rng = random.randint(1, 100)
+                # checks if the pixels is to be duplicated
+                if manipulate_pixel_chance_rng < chance:
+                    # Generate a random integer between 0 and the width-grid_size of the image
+                    x_start_rng = random.randint(0, width  - grid_size)
+                    y_start_rng = random.randint(height//2, height - grid_size)
+                    for new_y in range(y_start_rng,     y_start_rng + grid_size):
+                        for new_x in range(x_start_rng, x_start_rng + grid_size):
+                            # Append the duplicated pixel to the new_pixels list
+                            _new_pixels.append(img_pixels[new_y * width + new_x])
+                else:
+                    #pass
+                    _new_pixels.append(img_pixels[_y * width + _x])
+            pbar.update(1)
     return _new_pixels
 
 def combine_pixels(*pixel_lists) -> List:
@@ -270,24 +278,21 @@ def combine_pixels(*pixel_lists) -> List:
         else:
             print(f"{CORRECT} Combining pixels")
     combined_pixels = []
-    for img_pixels in tqdm(
-                            zip(*pixel_lists),
-                            desc="combining pixels",
-                            total=len(pixel_lists[0]),
-                            bar_format="{desc}:{percentage:3.0f}% |{bar}|",
-                            leave=False,
-                            disable=args.no_progress or args.quiet):
-        combined_pixel = [0, 0, 0]
-        # Loop over the pixels and add the corresponding color channels
-        for pixel in img_pixels:
-            # If the pixel is an integer, convert it to a 3-tuple with identical values
-            if isinstance(pixel, int):
-                pixel = (pixel, pixel, pixel)
-            # Add the pixel values to the combined pixel
-            combined_pixel = [sum(x) for x in zip(combined_pixel, pixel)]
-        # Divide the combined pixel values by the number of input lists to get the average
-        combined_pixel = tuple(x//len(pixel_lists) for x in combined_pixel)
-        combined_pixels.append(combined_pixel)
+    #for img_pixels in tqdm(zip(*pixel_lists),desc="combining pixels",total=len(pixel_lists[0]),bar_format="{desc}:{percentage:3.0f}% |{bar}|",leave=False,disable=args.no_progress or args.quiet):
+    with TqdmWrapper(desc="Combining Pixels", total=len(pixel_lists[0]), disable=args.no_progress or args.quiet) as pbar:
+        for img_pixels in zip(*pixel_lists):
+            combined_pixel = [0, 0, 0]
+            # Loop over the pixels and add the corresponding color channels
+            for pixel in img_pixels:
+                # If the pixel is an integer, convert it to a 3-tuple with identical values
+                if isinstance(pixel, int):
+                    pixel = (pixel, pixel, pixel)
+                # Add the pixel values to the combined pixel
+                combined_pixel = [sum(x) for x in zip(combined_pixel, pixel)]
+            # Divide the combined pixel values by the number of input lists to get the average
+            combined_pixel = tuple(x//len(pixel_lists) for x in combined_pixel)
+            combined_pixels.append(combined_pixel)
+            pbar.update(1)
     return combined_pixels
 
 def main():
@@ -337,9 +342,9 @@ def main():
     shift_pixels      = shift(im)
     duplicated_pixels = duplicate(im)
     pixels = combine_pixels(
-                            noise_pixels,
+                            #noise_pixels,
                             shift_pixels,
-                            duplicated_pixels,
+                            #duplicated_pixels,
                             )
     if args.verbose and not args.quiet:
         print(f"{current_time()}{VERBOSE_STRING} ADDING NEW DATA TO {new_image_file} [MAIN()]")
